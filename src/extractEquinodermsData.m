2fd2724dc89fd05a3f6dd6957e133af6ee9e48e5
anttagjan <antonio.tagua@hotmail.es>
2021-07-22 13:29:15 +0200

    Create extractEquinodermsData.m
    Extract:
    -Relative frequency of intercalations in each sample
    -Relative frequency of each interphase stage (average of all samples)
    -Absolute frequency of intercalations in each sample.
    -Absolute frequency of intercalations of all samples.
    -Relative frequency of each interphase stage (average of each sample)

---
+++ src/extractEquinodermsData.m
@@ -0,0 +1,30 @@
+function [allNumberMotives,allNumberMotivesNormalized,allTimeIntervals,allMiniataAfterMotives,allMiniataBeforeMotives,allMiniataInterMotives,allMiniataIndepMotives,totalPercentageMiniataMotives] = extractEquinodermsData(filename)
+
+load(fullfile(filename.folder, filename.name));
+movieFiles = whos('file','*movie*');
+allMiniataAfterMotives=zeros(length(movieFiles),3);
+allMiniataBeforeMotives=zeros(length(movieFiles),3);
+allMiniataInterMotives=zeros(length(movieFiles),3);
+allMiniataIndepMotives=zeros(length(movieFiles),3);
+allNumberMotives=zeros(length(movieFiles),4);
+allNumberMotivesNormalized=zeros(length(movieFiles),4);
+for indexMovies = 1:length(movieFiles)
+    fileName=movieFiles(indexMovies,1).name;
+    [numberMotives,AfterMotives,BeforeMotives,AllInterphaseMotives,IndependentMotives] = classifyMotives(eval(fileName));
+    allMiniataAfterMotives(indexMovies,:)=AfterMotives; 
+    allMiniataBeforeMotives(indexMovies,:)=BeforeMotives; 
+    allMiniataInterMotives(indexMovies,:)= AllInterphaseMotives;
+    allMiniataIndepMotives(indexMovies,:)= IndependentMotives;
+    allNumberMotives(indexMovies,:)=numberMotives;
+    allNumberMotivesNormalized(indexMovies,:)=numberMotives/sum(numberMotives);
+end
+
+allTimeIntervals = [allMiniataAfterMotives allMiniataBeforeMotives allMiniataInterMotives allMiniataIndepMotives];
+
+allMiniataAfterMotives(all(isnan(allMiniataAfterMotives),2),:)=[];
+allMiniataBeforeMotives(all(isnan(allMiniataBeforeMotives),2),:)=[];
+allMiniataInterMotives(all(isnan(allMiniataInterMotives),2),:)=[];
+allMiniataIndepMotives(all(isnan(allMiniataIndepMotives),2),:)=[];
+
+
+totalPercentageMiniataMotives= sum(allNumberMotives/sum(sum(allNumberMotives)),1);
