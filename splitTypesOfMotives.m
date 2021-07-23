2855fb50e290854066379dbd98087c5cdbb1c5b2
anttagjan <antonio.tagua@hotmail.es>
2021-07-23 16:04:28 +0200

    Create splitTypesOfMotives.m
    classify the data of the motives in the 4 types

---
+++ splitTypesOfMotives.m
@@ -0,0 +1,17 @@
+function [after,before,inter,indep] = splitTypesOfMotives(typesOfMotives)
+%UNTITLED4 Summary of this function goes here
+%   Detailed explanation goes here
+
+after=typesOfMotives(1:4:end,:);
+before=typesOfMotives(2:4:end,:);
+inter=typesOfMotives(3:4:end,:);
+indep=typesOfMotives(4:4:end,:);
+
+if isnan(after(:,1))
+    after2=after;
+    after=before;
+    before=after2;
+end
+
+end
+
